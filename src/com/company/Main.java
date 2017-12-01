7b4d5207d2ae2ac607db0b886534a11fdd794ef3
Sail338 <srihari.shankar1@gmail.com>
2017-12-01 09:37:46 -0500

    fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -191,29 +191,33 @@ public class Main {
 //            return render(model,"/public/analytics.html");
 //        });
 
-        post("/analytics", (request, response) -> {
+        get("/analytics", (request, response) -> {
 //            String city = request.queryParams("location");
 //            System.out.println(city);
             HashMap<String, Object> model = new HashMap<String, Object>();
-            String query1 = "SELECT drinkers.`Average BAC %` as BAC, (drinkers.`Current GPA` - drinkers.`Original GPA`) as GPA_difference";
+            String query1 = "SELECT drinkers.`Average BAC %` as BAC, (drinkers.`Current GPA` - drinkers.`Original GPA`) as GPA FROM drinkers";
             System.out.println(query1);
             ResultSet rs = helper.select(query1);
             List<Float> x_data = new ArrayList<Float>();
+            List<Float> y_data = new ArrayList<Float>();
             while(rs.next()) {
                 x_data.add(rs.getFloat("BAC"));
+                y_data.add(rs.getFloat("GPA"));
             }
             Float[] x_arr = new Float[x_data.size()];
             x_arr = x_data.toArray(x_arr);
             System.out.println(x_arr);
-            model.put("x", x_arr);
+            model.put("x", x_data);
 
-            List<Float> y_data = new ArrayList<Float>();
+
             while(rs.next()) {
-                y_data.add(rs.getFloat("GPA_difference"));
+                System.out.println("psu");
+                System.out.println(rs.getString("GPA"));
+                y_data.add(rs.getFloat("GPA"));
             }
             Float[] y_arr = new Float[y_data.size()];
             y_arr = y_data.toArray(y_arr);
-            model.put("y", y_arr);
+            model.put("y", y_data);
 
             return render(model,"/public/analytics.html");
 
